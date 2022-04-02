d034dc4394ae97ca0cd9057ee57ee964d51049ee
Jorge <jorge.toledo.6@hotmail.com>
2022-04-02 13:51:49 -0300

    Add project files.

---
+++ PrograAvanzadaVN5C1/Persona.cs
@@ -0,0 +1,82 @@
+﻿/* namespace => Espacio de Nombres 
+ * Sirve para agrupar Class en un mismo contexto 
+ Por ejemplo namespace Animales agruparia clases Perro, Gato, Raton */
+namespace PrograAvanzadaVN5C1
+{
+    /* Internal grado de Privacidad de la Clase 
+     internal => Clase sera visible para todas las clases que formen
+    parte del mismo ensamblado
+     */
+    public class Persona
+    {
+        /* Plantillas / Moldes para crear Objetos */
+        /* Un objeto es una Instancia */
+        /* Instancia es la existencia de algo */
+
+        /* La clase se conforma de Campos
+         Los campos pueden ser Atributos, Metodos, o Eventos
+         */
+
+        /* Todas las Clases heredan de la Clase Object */
+
+        /*Campos Privados */
+        private string _rut;
+        private string _name;
+        private string _apellido;
+        private int _edad;
+
+        /* Porperties prop => propiedades */
+        public string Rut { get; set; }
+        public string Name => $"{_name} {_apellido}";
+        public int Edad {
+            get { return _edad; }
+            set {
+                if (value < 0 || Edad > 150)
+                {
+                    throw new ArgumentOutOfRangeException
+                        ("La edad debe estar entre 0 y 150");
+                }
+                else
+                {
+                    _edad = value;
+                }
+            }
+        }
+
+
+        /* Constructor ctor */
+        public Persona(string rut)
+        {
+            _rut = rut;
+        }
+        public Persona(string rut, string nombre, string apellido)
+        {
+            _rut = rut;
+            _name = nombre;
+            _apellido = apellido;
+        }
+        public Persona(int rut)
+        {
+            
+        }
+
+        /* Metodos 
+         Representan comportamiento */
+
+        //Metodos simples VOID
+        public void Caminar()
+        {
+            _edad = 0;
+        }
+
+        //Metodos con retorno
+        public string verEdad()
+        {
+            return "aaa";
+        }
+
+        
+
+
+    }
+}
---
+++ PrograAvanzadaVN5C1/Program.cs
@@ -0,0 +1,9 @@
+﻿using PrograAvanzadaVN5C1;
+
+Console.WriteLine("Hello, World!");
+
+Persona Carlos = new Persona("1111");
+Persona Jorge = new Persona("1111", "Jorge", "Toledo");
+Persona Juan = new Persona(1);
+
+string edadJuan = Juan.Caminar();
