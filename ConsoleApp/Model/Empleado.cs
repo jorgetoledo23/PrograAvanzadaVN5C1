6d601e70ab731373ba1c37e3a0361f32bbd78a15
Jorge <jorge.toledo.6@hotmail.com>
2022-04-11 20:29:05 -0400

    a

---
+++ ConsoleApp/Model/Empleado.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp.Model
+{
+    public class Empleado
+    {
+        //Members (Miembros) =>
+        //Propiedades que definen caracteristicas
+        //Metodos que definen comportamiento
+        //Eventos* controlan interaccion
+
+        public string Rut
+        {
+            get { return Rut; }
+            set { Rut = value; }
+        }
+        // => public string Rut { get; set; }
+
+        private int _Edad;
+        public int Edad
+        {
+            get { return _Edad; }
+            set {
+                if (value > 0 && value < 150)
+                {
+                    _Edad = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException
+                        ("Edad debe estar entre 0 y 150");
+                }
+            }
+        }
+
+
+
+
+
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleApp.Model;
+
 Console.WriteLine("Hello, World!");
+
+int i;
+int j = 10; //Asignacion
+string Name = "Jorge";
+bool Bandera = false; // true
+double PI = Math.PI;
+char Caracter = 'a';
+
+var Caracter2 = 'b';
+var PI2 = Math.PI;
+
+string[] paises = new string[10];
+paises[0] = "Chile";
+
+List<string> paises2 = new List<string>();
+paises2.Add("Chile");
+paises2.ForEach(pais =>
+{
+    Console.WriteLine(pais);
+});
+
+if (paises2.Contains("Chile"))
+{
+    //Hacer Algo
+}
+string? paisBuscado = paises2.Find(pais => pais.Equals("Bolivia"));
+//paisBuscado = null;
+
+//Posibilidad de tener un valor null;
+int edad = 10;
+int? edad2 = null;
+
+
+
