7ba0422359e3a45b6d9ac6e7df30b8c2c276b696
Jorge <jorge.toledo.6@hotmail.com>
2022-05-09 20:22:59 -0400

    9 Mayo

---
+++ SistemaWeb/Controllers/ClaseController.cs
@@ -0,0 +1,148 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaWeb.Models;
+
+namespace SistemaWeb.Controllers
+{
+    public class ClaseController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ClaseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clase
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Clases.ToListAsync());
+        }
+
+        // GET: Clase/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clase = await _context.Clases
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (clase == null)
+            {
+                return NotFound();
+            }
+
+            return View(clase);
+        }
+
+        // GET: Clase/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clase/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Descripcion")] Clase clase)
+        {
+
+            _context.Add(clase);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Clase/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clase = await _context.Clases.FindAsync(id);
+            if (clase == null)
+            {
+                return NotFound();
+            }
+            return View(clase);
+        }
+
+        // POST: Clase/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Descripcion")] Clase clase)
+        {
+            if (id != clase.Id)
+            {
+                return NotFound();
+            }
+
+
+            try
+            {
+                _context.Update(clase);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClaseExists(clase.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        // GET: Clase/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var clase = await _context.Clases
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (clase == null)
+            {
+                return NotFound();
+            }
+
+            return View(clase);
+        }
+
+        // POST: Clase/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var clase = await _context.Clases.FindAsync(id);
+            _context.Clases.Remove(clase);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClaseExists(int id)
+        {
+            return _context.Clases.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SistemaWeb/Controllers/ProcesadorController.cs
@@ -0,0 +1,126 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SistemaWeb.Models;
+
+namespace SistemaWeb.Controllers
+{
+    public class ProcesadorController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProcesadorController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Procesador
+        public async Task<IActionResult> ProcesadorIndex()
+        {
+            return View(await _context.Procesadores.ToListAsync());
+        }
+
+        // GET: Procesador/Details/5
+        public async Task<IActionResult> DetailProcesador(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var procesador = await _context.Procesadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (procesador == null)
+            {
+                return NotFound();
+            }
+
+            return View(procesador);
+        }
+
+        // GET: Procesador/Create
+        public IActionResult CrearProcesador()
+        {
+            return View();
+        }
+
+        // POST: Procesador/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CrearProcesador(Procesador procesador)
+        {
+            _context.Add(procesador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(ProcesadorIndex));
+        }
+
+        // GET: Procesador/Edit/5
+        public async Task<IActionResult> EditarProcesador(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var procesador = await _context.Procesadores.FindAsync(id);
+            if (procesador == null)
+            {
+                return NotFound();
+            }
+            return View(procesador);
+        }
+
+        // POST: Procesador/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditarProcesador(Procesador procesador)
+        {
+            _context.Update(procesador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(ProcesadorIndex));
+        }
+
+        // GET: Procesador/Delete/5
+        public async Task<IActionResult> DelProcesador(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var procesador = await _context.Procesadores
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (procesador == null)
+            {
+                return NotFound();
+            }
+
+            return View(procesador);
+        }
+
+        // POST: Procesador/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DelProcesador(int id)
+        {
+            var procesador = await _context.Procesadores.FindAsync(id);
+            _context.Procesadores.Remove(procesador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(ProcesadorIndex));
+        }
+
+        private bool ProcesadorExists(int id)
+        {
+            return _context.Procesadores.Any(e => e.Id == id);
+        }
+    }
+}
