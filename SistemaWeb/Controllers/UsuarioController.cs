0a31c9c9049b91a1ad2e0615d45b6f8ff49f36fc
Jorge <jorge.toledo.6@hotmail.com>
2022-06-13 20:25:18 -0400

    Gestion

--- SistemaWeb/Controllers/UsuarioController.cs
+++ SistemaWeb/Controllers/UsuarioController.cs
@@ -28,6 +28,7 @@ namespace SistemaWeb.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateUsuario(RegistroViewModel Rvm)
         {
+            //TODO: Validar que no se repita el Username y Correo en el crear Usuario
             if (ModelState.IsValid)
             {
                 Usuario? U = _context.Usuarios.Where(x => x.Username == Rvm.Username).FirstOrDefault();
@@ -63,6 +64,48 @@ namespace SistemaWeb.Controllers
             }
             
         }
+
+        public async Task<IActionResult> EditUsuario(Guid userid)
+        {
+            var Usuario = await _context.Usuarios.FindAsync(userid);
+            if (Usuario == null) return NotFound();
+            UserEditViewModel Uevm = new UserEditViewModel()
+            {
+                Username = Usuario.Username,
+                Email = Usuario.Email,
+                Id = userid,
+                Name = Usuario.Name,
+                Rol = Usuario.Rol
+            };
+            return View(Uevm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsuario(UserEditViewModel? Uevm)
+        {
+            //TODO: Validar que no se repita el Username y Correo en el Editar
+            if (!ModelState.IsValid) return View(Uevm);
+            var Usuario = await _context.Usuarios.FindAsync(Uevm.Id);
+            if (Usuario == null) return NotFound();
+
+            Usuario.Username = Uevm.Username;
+            Usuario.Email = Uevm.Email;
+            Usuario.Name = Uevm.Name;
+            Usuario.Rol = Uevm.Rol;
+
+            CreatePasswordHash(Uevm.Password, out byte[] Hash, out byte[] Salt);
+            Usuario.PasswordHash = Hash;
+            Usuario.PasswordSalt = Salt;
+
+            _context.Usuarios.Update(Usuario);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(GestionUsuarios));
+        }
+
+
+
+
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
